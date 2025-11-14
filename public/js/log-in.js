e364684e15efebef49e682aa2673f4feaf74d22d
Shree <shrutika.blacktrounce@gmail.com>
2025-11-14 17:50:32 +0530

    Created a log in page. Secured the index page using firebase authentification.

---
+++ public/js/log-in.js
@@ -0,0 +1,15 @@
+// LOGIN WITH FIREBASE AUTH
+function loginUser() {
+    const email = document.getElementById("emailInput").value;
+    const password = document.getElementById("passwordInput").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Login successful → GO TO ADMIN PAGE
+        window.location.href = "../public/views/index.html";
+    })
+    .catch((error) => {
+        document.getElementById("error-msg").innerText = "Invalid email or password.";
+        console.error(error);
+    });
+}
